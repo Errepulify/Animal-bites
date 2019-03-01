95bc9d66434b9b8e4e7ac350a545c5d6937d5295
Geert van Kaathoven <geertvk89@hotmail.com>
2019-03-01 20:51:19 +0000

    Made sure the data was used the right way

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,33 +1,40 @@
 queue()
     .defer(d3.csv, "data/animalBites.csv")
     .await(makeGraphs);
-    
-function makeGraphs(error, yearData,) {
+
+function makeGraphs(error, yearData, ) {
     var ndx = crossfilter(yearData);
-    
-   
+
+
     show_bite_balance(ndx);
-     
-    
-   
+    show_breed_selector(ndx);
+
+
+
 
     dc.renderAll();
+}
+
+
+
+function show_breed_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('breed'));
+    var group = dim.group();
+
+    dc.selectMenu("#breed-selector")
+        .dimension(dim)
+        .group(group);
 }
 
 function show_bite_balance(ndx) {
     var dim = ndx.dimension(dc.pluck('gender'));
     var group = dim.group().reduceSum(dc.pluck('bite'));
-   
-    
-    
-    
-    
-    
-    
-    dc.barChart("#total-bites-per-animal")
+
+
+    dc.barChart("#total-bites-per-gender")
         .width(600)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 80})
+        .margins({ top: 10, right: 50, bottom: 30, left: 80 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -36,7 +43,4 @@ function show_bite_balance(ndx) {
         .xAxisLabel("Animal")
         .yAxisLabel("Total")
         .yAxis().ticks(20);
-        
-        
-       
 }

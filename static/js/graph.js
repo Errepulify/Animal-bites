45493a715f71b4796bf09432991c8060c9c8043b
Geert van Kaathoven <geertvk89@hotmail.com>
2019-03-03 13:37:44 +0000

    Added function under the selector to show the text 'Choose your breed' on the dashboard

--- static/js/graph.js
+++ static/js/graph.js
@@ -1,7 +1,10 @@
+/*Getting data from the csv file*/
+
 queue()
     .defer(d3.csv, "data/animalBites.csv")
     .await(makeGraphs);
 
+/*Creating all the graphs*/
 function makeGraphs(error, yearData, ) {
     var ndx = crossfilter(yearData);
 
@@ -16,21 +19,26 @@ function makeGraphs(error, yearData, ) {
 }
 
 
-
+/*Dog breed selector menu*/
 function show_breed_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('breed'));
     var group = dim.group();
 
     dc.selectMenu("#breed-selector")
         .dimension(dim)
-        .group(group);
+        .group(group)
+         .title(function (d) {
+            return d.key;
+        })
+        .promptText("Choose your breed:");
+        
 }
 
 function show_bite_balance(ndx) {
     var dim = ndx.dimension(dc.pluck('gender'));
     var group = dim.group().reduceSum(dc.pluck('bite'));
 
-
+/*creates barchart total bites per gender*/
     dc.barChart("#total-bites-per-gender")
         .width(600)
         .height(300)
@@ -40,7 +48,7 @@ function show_bite_balance(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("Animal")
+        .xAxisLabel("Gender")
         .yAxisLabel("Total")
         .yAxis().ticks(20);
 }

c334480573c0cb0e901d463f12f636f84c40ec44
Geert van Kaathoven <geertvk89@hotmail.com>
2019-03-03 20:50:18 +0000

    Merged the other js with this one so crossfilter works

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,9 +8,12 @@ queue()
 function makeGraphs(error, yearData, ) {
     var ndx = crossfilter(yearData);
 
-
-    show_bite_balance(ndx);
     show_breed_selector(ndx);
+    show_breed2_selector(ndx);
+    show_bar_chart(ndx);
+    show_where_balance(ndx);
+    
+   
 
 
 
@@ -27,19 +30,27 @@ function show_breed_selector(ndx) {
     dc.selectMenu("#breed-selector")
         .dimension(dim)
         .group(group)
-         .title(function (d) {
-            return d.key;
-        })
-        .promptText("Choose your breed:");
+       
+        
+}
+/*Second dog breed selector menu*/
+function show_breed2_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('breed'));
+    var group = dim.group();
+
+    dc.selectMenu("#breed2-selector")
+        .dimension(dim)
+        .group(group)
+        
         
 }
 
-function show_bite_balance(ndx) {
-    var dim = ndx.dimension(dc.pluck('gender'));
+function show_bar_chart(ndx) {
+    var dim = ndx.dimension(dc.pluck('date'));
     var group = dim.group().reduceSum(dc.pluck('bite'));
 
 /*creates barchart total bites per gender*/
-    dc.barChart("#total-bites-per-gender")
+    dc.barChart("#total-bites-per-breed")
         .width(600)
         .height(300)
         .margins({ top: 10, right: 50, bottom: 30, left: 80 })
@@ -48,7 +59,28 @@ function show_bite_balance(ndx) {
         .transitionDuration(500)
         .x(d3.scale.ordinal())
         .xUnits(dc.units.ordinal)
-        .xAxisLabel("Gender")
+        .xAxisLabel("Year")
         .yAxisLabel("Total")
         .yAxis().ticks(20);
 }
+
+function show_where_balance(ndx) {
+    var dim = ndx.dimension(dc.pluck('wherebitten'));
+    var group = dim.group().reduceSum(dc.pluck('bite'));
+
+
+    dc.barChart("#where-chart")
+        .width(600)
+        .height(300)
+        .margins({ top: 10, right: 50, bottom: 30, left: 80 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Where")
+        .yAxisLabel("Total")
+        .yAxis().ticks(20);
+}
+
+
